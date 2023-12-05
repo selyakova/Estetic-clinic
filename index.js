b5b156cfccd10876abc885e21e551d67f6f1963e
Opilane TTHK <opilane@tthk.ee>
2023-12-05 18:06:53 +0200

    As an app developer I want to be able to add a new service [Deliver #186567162]

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const app = require('express')();
+const express = require('express')
+const app = express()
+
+//const app = require('express')();
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 //const swaggerDocument = require('./docs/swagger.json');
+
+
 
 const estetic = [
     {id: 1, name: "Botox", price:250},
@@ -11,15 +16,15 @@ const estetic = [
     {id: 3, name:"Manual teraphy", price:90}
 ]
 
-app.get('/Estetic-clinic', (req, res) => {
+app.get('/Esteticclinic', (req, res) => {
     res.send(estetic)
 })
 
-app.get('/Estetic-clinic', (req, res) => {
+app.get('/Esteticclinic', (req, res) => {
     res.send(estetic)
 })
 
-app.get('/Estetic-clinic/:id', (req,res) => {
+app.get('/Esteticclinic/:id', (req,res) => {
     if (typeof estetic[req.params.id -1] === 'undifined')
     {
         return res.status(404).send({error: "Service not found"})
@@ -27,17 +32,34 @@ app.get('/Estetic-clinic/:id', (req,res) => {
     res.send(estetic[req.params.id - 1])
 })
 
-app.post('/Estetic-clinic', (req, res) => {
-    estetic.push({
+app.post('/Esteticclinic', (req, res) => {
+    if (!req.body.name || !req-body.price) {
+        return res.status(400).send({ error: 'One or all params are missing' })
+    }
+    let estserv = { 
         id: estetic.length +1,
         price: req.body.price,
         name: req.body.name
-    })
-    res.end()
+    }
+    estetic.push(estserv)
+    res.status(201)
+    .location(`${getBaseUrl(req)}/esteticclinic/${estetic.length}`)
+    .send(estserv)
 })
+
+
+
+app.use(express.json())
+
+//app.delete('/esteticclinic/:id',)
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+    ? 'https' : 'http' + `://${req.headers.host}`
+}
