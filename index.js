d9285e187228f3df8aa624dd8cef5ce6ae556a99
Opilane TTHK <opilane@tthk.ee>
2023-12-05 14:41:12 +0200

    As an app developer I want to get a list of all estetic clinic services to show in my app [Delivers #186567032]

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/Estetic-clinic', (req, res) => {
+    res.send(["Botox", "LPG massage", "Manual teraphy"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
