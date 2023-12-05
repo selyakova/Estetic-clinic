a6e4d0d7705e86a1b249393b52794915e766c24a
Opilane TTHK <opilane@tthk.ee>
2023-12-05 16:43:40 +0200

    As an app developer I want to get a full details of a given estetic clinic to show in my app. [Delivers #186567091]

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const app = require('express')();
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+//const swaggerDocument = require('./docs/swagger.json');
+
+const estetic = [
+    {id: 1, name: "Botox", price:250},
+    {id: 2, name:"LPG massage", price:70},
+    {id: 3, name:"Manual teraphy", price:90}
+]
 
 app.get('/Estetic-clinic', (req, res) => {
-    res.send(["Botox", "LPG massage", "Manual teraphy"])
+    res.send(estetic)
+})
+
+app.get('/Estetic-clinic', (req, res) => {
+    res.send(estetic)
+})
+
+app.get('/Estetic-clinic/:id', (req,res) => {
+    if (typeof estetic[req.params.id -1] === 'undifined')
+    {
+        return res.status(404).send({error: "Service not found"})
+    }
+    res.send(estetic[req.params.id - 1])
+})
+
+app.post('/Estetic-clinic', (req, res) => {
+    estetic.push({
+        id: estetic.length +1,
+        price: req.body.price,
+        name: req.body.name
+    })
+    res.end()
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
